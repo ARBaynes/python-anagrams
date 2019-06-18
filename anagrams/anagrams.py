dcc8683fe2d3b54932cf18ba506eebef3f0b40cb
Annabel <annabel.baynes@gmail.com>
2019-06-18 02:06:02 +0100

    Added another thread lock for cautions sake (I may be being overcautious here...)

--- anagrams/anagrams.py
+++ anagrams/anagrams.py
@@ -16,7 +16,15 @@ class Anagrams:
             lock.release()
 
     def get_anagrams(self, word):
-        sorted_word = sort_string(word)
-        sorted_list_of_words = sorted_list_of_strings(self.words)
-        return [word_tuple[0] for word_tuple in sorted_list_of_words if word_tuple[1] == sorted_word]
+        lock = threading.RLock()
+
+        lock.acquire()
+        try:
+            sorted_word = sort_string(word)
+            sorted_list_of_words = sorted_list_of_strings(self.words)
+            anagram_list = [word_tuple[0] for word_tuple in sorted_list_of_words if word_tuple[1] == sorted_word]
+        finally:
+            lock.release()
+            
+        return anagram_list
 
