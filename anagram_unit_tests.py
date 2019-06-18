32885aafd2b7eba1dfcd999846c336911923a5c9
Annabel <annabel.baynes@gmail.com>
2019-06-18 02:02:02 +0100

    Added separate string sorting methods for unit testing
    File read is now safer
    Added anagram-searching logic

---
+++ anagram_unit_tests.py
@@ -0,0 +1,21 @@
+import unittest
+from anagrams.anagrams import Anagrams
+from anagrams.string_sorts import sort_string
+
+
+class TestAnagrams(unittest.TestCase):
+
+    def test_anagrams(self):
+        anagrams = Anagrams()
+        self.assertEqual(anagrams.get_anagrams('plates'), ['palest', 'pastel', 'petals', 'plates', 'staple'])
+        self.assertEqual(anagrams.get_anagrams('eat'), ['ate', 'eat', 'tea'])
+
+
+class TestStringSorter(unittest.TestCase):
+
+    def test_string_sorter(self):
+        self.assertEqual(sort_string('Consume'), ['c', 'e', 'm', 'n', 'o', 's', 'u'])
+
+
+if __name__ == '__main__':
+    unittest.main()
